c942128afb09f037ab5ba155070fdc3c7dde1e7d
JuliaShrestha <juliashrestha215@gmail.com>
2024-08-31 13:52:31 +0300

    Class-calculator

---
+++ index.js
@@ -0,0 +1,49 @@
+/*
+Створіть клас Calculator, який буде мати методи для виконання базових математичних операцій: додавання, віднімання, множення та ділення.
+
+class Calculator {
+
+ // Ваш код тут
+
+}
+
+const calc = new Calculator();
+
+console.log(calc.add(5, 3)); // 8
+
+console.log(calc.subtract(10, 4)); // 6
+
+console.log(calc.multiply(3, 6)); // 18
+
+console.log(calc.divide(8, 2)); // 4
+*/
+
+class Calculator {
+  
+    add(a, b) {
+      return a + b;
+    }
+   
+    subtract(a, b) {
+      return a - b;
+    }
+  
+    multiply(a, b) {
+      return a * b;
+    }
+    
+    divide(a, b) {
+      
+      if (b === 0) {
+        return 'Division by zero is not allowed';
+      }
+      return a / b;
+    }
+  }
+  
+  const calc = new Calculator();
+  
+  console.log(calc.add(5, 3));
+  console.log(calc.subtract(10, 4)); 
+  console.log(calc.multiply(3, 6)); 
+  console.log(calc.divide(8, 2)); 
